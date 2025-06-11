0eed5358da37fbc9bf5680bee89af14f75c36a8c
Sambhav  Gupta <sambhavgupta0705@gmail.com>
2025-06-12 00:26:27 +0530

    initial again

--- components/community/HomeCard.tsx
+++ components/community/HomeCard.tsx
@@ -17,7 +17,7 @@ interface HomeCardProps {
 
 /**
  * @description This component displays  Card.
- * @param {HomeCardProps} props - The props  Home Card component.
+ * @param {HomeCardProps} props - The   Home Card component.
  * @param {string} props.headline - The  for the home card.
  * @param {string} props.title - The.
  * @param {string} props.description - The  for the home card.
